ef4db2afe91a7a62b833520d0759ee5b72361e1a
Paulo <paulorolinski.ifc@gmail.com>
2022-10-02 17:59:39 -0300

    Eros v1.0

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createVuesticEssential, VaButton } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import 'vuestic-ui/styles/grid.css'
+import 'vuestic-ui/styles/reset.css'
+import 'vuestic-ui/styles/typography.css'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(createVuesticEssential({ components: { VaButton } })).use(createVuestic()).mount('#app')
